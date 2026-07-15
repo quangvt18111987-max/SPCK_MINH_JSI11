a95d9157144834a6a3b69bdc92dafccf1f4546d8
Banhtalon <Banhtalon@users.noreply.github.com>
2026-07-15 20:47:06 +0700

    Thêm cấu hình Firebase thật

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -1,11 +1,12 @@
 // Firebase Configuration - Using compat (v10) SDK
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDTNtz5SfUtWr4ZktE_4V--VVVBo4vPrJM",
+    authDomain: "spck-89d8c.firebaseapp.com",
+    projectId: "spck-89d8c",
+    storageBucket: "spck-89d8c.firebasestorage.app",
+    messagingSenderId: "473190019268",
+    appId: "1:473190019268:web:71f89b047140f0061bd991",
+    measurementId: "G-7DHW507QHX"
 };
 
 // Initialize Firebase
